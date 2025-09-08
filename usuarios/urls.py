613448958cfb6163e3f85f5c2eba4e9353dae0b4
Sebastian Furquet <Sebastianfurquet@gmail.com>
2025-09-07 23:30:57 -0300

    login, logout, registro

---
+++ usuarios/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from usuarios.views import registro, login_request, Logout
+#from django.contrib.auth import views as auth_views 
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('login/', login_request, name='login'),
+    #path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'), # le deberia poner a donde me redirige 
+    path('logout/', LogoutView.as_view(template_name= 'usuarios/logout.html'), name='logout'), 
+    path('registro/', registro, name='registro'),
+]
